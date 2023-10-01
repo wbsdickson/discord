c4d23b3e3fa25925433bcefda7da061ef825f702
Dickson Wu <wbs.dickson@gmail.com>
2023-09-30 20:56:08 -0600

    add create server modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
@@ -24,6 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                         storageKey="doscird-theme"
                         disableTransitionOnChange
                     >
+                        <ModalProvider />
                         {children}{" "}
                     </ThemeProvider>
                 </body>
