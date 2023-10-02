0a295676dce264d7231b8a8923c8b14c67421567
Dickson Wu <wbs.dickson@gmail.com>
2023-10-02 04:09:05 -0600

    add emoji and attachment modal in chat

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -4,7 +4,7 @@ import { ChannelType } from "@prisma/client";
 
 import { currentProfile } from "@/lib/current-profile";
 import { ChatHeader } from "@/components/chat/chat-header";
-// import { ChatInput } from "@/components/chat/chat-input";
+import { ChatInput } from "@/components/chat/chat-input";
 // import { ChatMessages } from "@/components/chat/chat-messages";
 // import { MediaRoom } from "@/components/media-room";
 import { db } from "@/lib/db";
@@ -43,9 +43,9 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
     return (
         <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
             <ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
-            {/* {channel.type === ChannelType.TEXT && (
-        <>
-          <ChatMessages
+            {channel.type === ChannelType.TEXT && (
+                <>
+                    {/* <ChatMessages
             member={member}
             name={channel.name}
             chatId={channel.id}
@@ -58,26 +58,26 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
             }}
             paramKey="channelId"
             paramValue={channel.id}
-          />
-          <ChatInput
-            name={channel.name}
-            type="channel"
-            apiUrl="/api/socket/messages"
-            query={{
-              channelId: channel.id,
-              serverId: channel.serverId,
-            }}
-          />
-        </>
-      )}
-      {channel.type === ChannelType.AUDIO && (
+          /> */}
+                    <ChatInput
+                        name={channel.name}
+                        type="channel"
+                        apiUrl="/api/socket/messages"
+                        query={{
+                            channelId: channel.id,
+                            serverId: channel.serverId,
+                        }}
+                    />
+                </>
+            )}
+            {/* {channel.type === ChannelType.AUDIO && (
         <MediaRoom
           chatId={channel.id}
           video={false}
           audio={true}
         />
-      )}
-      {channel.type === ChannelType.VIDEO && (
+      )} */}
+            {/* {channel.type === ChannelType.VIDEO && (
         <MediaRoom
           chatId={channel.id}
           video={true}
