ada620cfc9e35355854ce31e6204848051284605
Dickson Wu <wbs.dickson@gmail.com>
2023-10-01 22:41:00 -0600

    add socket client and socket indicator

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -2,7 +2,7 @@ import { Hash } from "lucide-react";
 
 import { MobileToggle } from "@/components/mobile-toggle";
 import { UserAvatar } from "@/components/user-avatar";
-// import { SocketIndicator } from "@/components/socket-indicator";
+import { SocketIndicator } from "@/components/socket-indicator";
 
 import { ChatVideoButton } from "./chat-video-button";
 
@@ -22,7 +22,7 @@ export const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) 
             <p className="font-semibold text-md text-black dark:text-white">{name}</p>
             <div className="ml-auto flex items-center">
                 {type === "conversation" && <ChatVideoButton />}
-                {/* <SocketIndicator /> */}
+                <SocketIndicator />
             </div>
         </div>
     );
